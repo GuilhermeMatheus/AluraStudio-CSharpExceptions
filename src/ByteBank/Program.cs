252ed8efe892abab7861a8c0756380833c94dc85
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-05-16 17:09:42 -0300

    Video 2.1

--- src/ByteBank/Program.cs
+++ src/ByteBank/Program.cs
@@ -18,22 +18,34 @@ namespace ByteBank
 
         private static void Metodo()
         {
-            try
-            { 
-                Console.WriteLine("TRY: inicio");
-                TestaDivisao(0);
-                TestaDivisao(20);
-                Console.WriteLine("TRY: fim");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Ops, erro DivideByZeroException");
-            }
+            TestaDivisao(0);
+            //try
+            //{ 
+            //    Console.WriteLine("TRY: inicio");
+            //    TestaDivisao(0);
+            //    TestaDivisao(20);
+            //    Console.WriteLine("TRY: fim");
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Mensagem: " + ex.Message);
+            //    Console.WriteLine("Rastro da pilha: " + ex.StackTrace);
+            //    Console.WriteLine("Ops, erro DivideByZeroException");
+            //}
         }
 
         private static void TestaDivisao(int divisor)
         {
-            Dividir(10, divisor);
+            try
+            {
+                Dividir(10, divisor);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.WriteLine("Estou executando normalmente!");
+
         }
 
         private static int Dividir(int numero, int divisor)
