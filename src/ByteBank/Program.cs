5aaeeebd001c84b970e6225013e546f847b4a069
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-05-24 18:57:25 -0300

    Video 5.2

--- src/ByteBank/Program.cs
+++ src/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,51 @@ namespace ByteBank
     class Program
     {
         static void Main(string[] args)
+        {
+            try
+            {
+                CarregarContas();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+
+
+            // ---------------------------------------------
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contasl.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+
+        private static void TestaInnerException()
         {
             try
             {
@@ -24,73 +70,34 @@ namespace ByteBank
                 Console.WriteLine(e.StackTrace);
 
                 // Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-                
-            }
 
-            Console.WriteLine("Execução finalizada. Tecle enter para sair");
-            Console.ReadLine();
+            }
         }
 
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
         {
             TestaDivisao(0);
-            //try
-            //{ 
-            //    Console.WriteLine("TRY: inicio");
-            //    TestaDivisao(0);
-            //    TestaDivisao(20);
-            //    Console.WriteLine("TRY: fim");
-            //}
-            //catch (DivideByZeroException ex)
-            //{
-            //    Console.WriteLine("Mensagem: " + ex.Message);
-            //    Console.WriteLine("Rastro da pilha: " + ex.StackTrace);
-            //    Console.WriteLine("Ops, erro DivideByZeroException");
-            //}
         }
 
         private static void TestaDivisao(int divisor)
         {
-            try
-            {
-                Dividir(10, divisor);
-            }
-            catch (NullReferenceException ex)
-            {
-                Console.WriteLine("Fui capturado pelo (NullReferenceException ex)");
-                Console.WriteLine(ex.StackTrace);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Fui capturado pelo (Exception ex)");
-                Console.WriteLine(ex.StackTrace);
-            }
-            
-            //catch(DivideByZeroException ex)
-            //{
-            //    Console.WriteLine(ex.StackTrace);
-            //}
-
-            Console.WriteLine("Estou executando normalmente!");
-
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
         }
 
         private static int Dividir(int numero, int divisor)
         {
             try
             {
-                Console.WriteLine("Fazendo o cálculo " + numero + " / " + divisor);
-                int resultado = numero / divisor;
-                Console.WriteLine("O resultado é " + resultado);
-                return resultado;
+                return numero / divisor;
             }
-            catch(Exception ex)
+            catch (DivideByZeroException )
             {
-                Console.WriteLine("Erro no cálculo: " + ex.Message);
-                throw; // Lançar exceção
-
-                Console.WriteLine("Depois do throw");
-
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+                Console.WriteLine("Código depois do throw");
             }
         }
 
